88c690f368a7291e3b236c5bc971544e70596875
clvrk <52180876+clvrk@users.noreply.github.com>
2020-09-09 16:27:46 -0400

    Update flame.py

--- flame.py
+++ flame.py
@@ -65,7 +65,7 @@ async def on_member_join(member):
     channel2 = get(member.guild.channels, name = 'rules')
     mbed = discord.Embed(
         title = 'Welcome',
-        description = f'Hello {member.mention}, welcome to Polaroid, please head over to {channel2.mention} to verify yourself!',
+        description = f'Hello {member.mention}, welcome to Polaroid! Please head over to {channel2.mention} to verify yourself.',
         color = discord.Color(0xe3a2fc)
     )
     mbed.set_image(url = 'https://cdn.discordapp.com/avatars/732654582089515009/8506972331cc82af0b5ef4d6f2dff59b.webp?size=1024')
