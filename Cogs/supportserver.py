6b3db69f75409c2dd839142717b14fe303510118
clvrk <jheru711@Gmail.com>
2021-01-28 14:22:22 -0400

    Added uptime + fixed typo

--- Cogs/supportserver.py
+++ Cogs/supportserver.py
@@ -1,13 +1,14 @@
 import discord
 from discord.ext import commands
 import random
-
+import time
+from datetime import datetime
 colors = [0xe3a2fc, 0x0da2ff]
 
 class supportserver(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+        self.bot.launch_time = datetime.utcnow()
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -32,24 +33,30 @@ class supportserver(commands.Cog):
     @commands.command(aliases=['s'])
     async def stats(self, ctx):
         if ctx.guild.id == 687177202823069697:
-            before = time.monotonic()
-            message = await ctx.send('Pinging...')
-            ping = (time.monotonic() - before) * 1000
-            if ping < 200:
-                color = 0x35fc03
-            elif ping < 350:
-                color = 0xe3f51d
-            elif ping < 500:
-                color = 0xf7700f
-            else:
-                color = 0xf7220f
-            pEmbed = discord.Embed(title="Stats.", color=color)
-            pEmbed.add_field(name="Latency", value=f'{int(ping)}ms')
-            pEmbed.set_author(name=f'{ctx.guild.member_count}', icon=ctx.guild.icon_url)
-            pEmbed.add_field(name="API", value=f'{round(self.bot.latency * 1000)}ms')
-            pEmbed.set_thumbnail(url=self.bot.user.avatar_url)
-            await message.edit(content=None, embed=pEmbed)
-            await ctx.send(embed=mbed)
+            if ctx.author.guild_permissions.administrator:
+                uptime = datetime.utcnow() - self.bot.launch_time
+                hours, remainder = divmod(int(uptime.total_seconds()), 3600)
+                minutes, seconds = divmod(remainder, 60)
+                days, hours = divmod(hours, 24)
+                before = time.monotonic()
+                message = await ctx.send('Pinging...')
+                ping = (time.monotonic() - before) * 1000
+                if ping < 200:
+                    color = 0x35fc03
+                elif ping < 350:
+                    color = 0xe3f51d
+                elif ping < 500:
+                    color = 0xf7700f
+                else:
+                    color = 0xf7220f
+                pEmbed = discord.Embed(title="Stats.", color=color)
+                pEmbed.add_field(name="Latency", value=f'{int(ping)}ms')
+                pEmbed.set_author(name=f'{ctx.guild.member_count}', icon=ctx.guild.icon_url)
+                pEmbed.add_field(name="API", value=f'{round(self.bot.latency * 1000)}ms')
+                pEmbed.add_field(name="Uptime", value=f"Taking photos for {days}d, {hours}h, {minutes}m")
+                pEmbed.set_thumbnail(url=self.bot.user.avatar_url)
+                await message.edit(content=None, embed=pEmbed)
+                await ctx.send(embed=mbed)
 
 def setup(bot):
     bot.add_cog(supportserver(bot))
