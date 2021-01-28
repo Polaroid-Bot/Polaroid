5caedb51afd592b6eefbcbbbb5a446f0d0872e51
clvrk <52180876+clvrk@users.noreply.github.com>
2021-01-28 04:17:37 -0800

    Update filters.py

--- Cogs/filters.py
+++ Cogs/filters.py
@@ -1,10 +1,10 @@
 import discord
 from discord.ext import commands
-from PIL import ImageFilters
+from PIL import ImageFilter
 
-class supportserver(commands.Cog):
+class filters(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
 def setup(bot):
-    bot.add_cog(supportserver(bot))
+    bot.add_cog(filters(bot))
