b7fcaf7536200624b865c82efbde48a2c62f642b
clvrk <jheru711@Gmail.com>
2021-01-28 11:53:05 -0400

    Added blur filter

--- Cogs/filters.py
+++ Cogs/filters.py
@@ -5,6 +5,7 @@ from PIL import Image
 from discord.ext.commands import BucketType
 from io import BytesIO
 import requests
+import numpy as np
 
 class filters(commands.Cog):
     def __init__(self, bot):
@@ -13,7 +14,16 @@ class filters(commands.Cog):
     @commands.command()
     @commands.cooldown(rate=1, per=2, type=BucketType.user)
     async def blur(self, ctx, url: str):
-        
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content), mode='r')
+        im = img.filter(ImageFilter.BLUR)
+        b = BytesIO()
+        im.save(b, format='PNG')
+        byte_im = b.getvalue()
+        with open('blur.png','wb')as img:
+            img.write(byte_im)
+            await ctx.send(file=discord.File("blur.png"))
+        os.remove("blur.png")
 
 
 def setup(bot):
