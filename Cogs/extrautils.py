bbd3fea6607b1ef7e258e3070eea2c42149df0fe
clvrk <52180876+clvrk@users.noreply.github.com>
2021-01-31 16:37:14 -0800

    typo

--- Cogs/extrautils.py
+++ Cogs/extrautils.py
@@ -38,7 +38,7 @@ class extrautils(commands.Cog):
                 color=random.choice(colors)
             )
             mbed.url = f'{ctx.author.avatar_url}'
-            mbed.set_author(name=f'{ctx.author.name} | Requested by {ctx.author.mention}', icon_url=f'{user.avatar_url}')
+            mbed.set_author(name=f'{ctx.author.name} | Requested by {ctx.author.mention}', icon_url=f'{ctx.author.avatar_url}')
             mbed.set_image(url=f"{ctx.author.avatar_url}")
             mbed.set_footer(text=f'Syntax: p!av <image link>')
             await ctx.send(embed=mbed)
