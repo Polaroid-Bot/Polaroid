b35f5a8f76c7524b2d6ac2fbab68c00950f9b4e3
clvrk <jheru711@Gmail.com>
2021-02-13 14:26:29 -0400

    Finished up webp handling

--- Cogs/manipulation.py
+++ Cogs/manipulation.py
@@ -51,9 +51,9 @@ class manipulation(commands.Cog):
             await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Error when making request. | Image may be a WEBP file', color=color))
 
 
-    @commands.command(aliases=['5g1g'])
+    @commands.command(aliases=['fiveguysonegirl', '5g1g'])
     @commands.cooldown(rate=2, per=3, type=BucketType.user)
-    async def fiveguysonegirl(self, ctx, url1: str, url2: str):
+    async def fiveg1g(self, ctx, url1: str, url2: str):
         async with self.ses.get(f'https://api.dagpi.xyz/image/5g1g/?url={url1}&url2={url2}', headers={'Authorization': self.dag_token}) as r:
             if r.status in range(200, 299):
                 img = Image.open(BytesIO(await r.read()), mode='r')
@@ -115,19 +115,62 @@ class manipulation(commands.Cog):
             await ctx.send(embed=errembed)
 
         elif isinstance(error, commands.MissingRequiredArgument):
-            url = str(ctx.author.avatar_url)
-            try:
-                img = await self.dagpi.image_process(ImageFeatures.magik(), url)
-                mbed = discord.Embed(
-                    title='Snap!',
-                    color=color
-                )
-                mbed.set_image(url=f"attachment://magik.{img.format}")
-                mbed.set_footer(text=f'Syntax: p! magik <image link> | Quality may be bad. Specify url if this is the case.')
-                file = discord.File(fp=img.image, filename=f"magik.{img.format}")
-                await ctx.send(embed=mbed, file=file)
-            except:
-                await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Error when making request.', color=color))
+            async with self.ses.get(str(ctx.author.avatar_url)) as r:
+                img = Image.open(BytesIO(await r.read()), mode='r')
+                try:
+                    img.seek(1)
+                    url = str(ctx.author.avatar_url_as(format=f'gif'))
+                except EOFError:
+                    url = str(ctx.author.avatar_url_as(format=f'png'))
+
+                try:
+                    img = await self.dagpi.image_process(ImageFeatures.magik(), url)
+                    mbed = discord.Embed(
+                        title='Snap!',
+                        color=color
+                    )
+                    mbed.set_image(url=f"attachment://magik.{img.format}")
+                    mbed.set_footer(text=f'Syntax: p! magik <image link> | Quality may be bad. Specify url if this is the case.')
+                    file = discord.File(fp=img.image, filename=f"magik.{img.format}")
+                    await ctx.send(embed=mbed, file=file)
+                except:
+                    await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Error when making request.', color=color))
+
+    @fiveg1g.error
+    async def fiveg1g_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            errembed = discord.Embed(
+                title='Hold on there, buddy',
+                color=err_color,
+                description='Wait 3 more seconds before you can get another snap!'
+            )
+            await ctx.send(embed=errembed)
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            async with self.ses.get(str(ctx.author.avatar_url)) as r:
+                img = Image.open(BytesIO(await r.read()), mode='r')
+                try:
+                    img.seek(1)
+                    url = str(ctx.author.avatar_url_as(format=f'gif'))
+                except EOFError:
+                    url = str(ctx.author.avatar_url_as(format=f'png'))
+
+                async with self.ses.get(f'https://api.dagpi.xyz/image/5g1g/?url={url}&url2={url}', headers={'Authorization': self.dag_token}) as r:
+                    if r.status in range(200, 299):
+                        img = Image.open(BytesIO(await r.read()), mode='r')
+                        b = BytesIO()
+                        img.save(b, format=f'{img.format}')
+                        b_im = b.getvalue()
+                        file = discord.File(filename=f'5g1g.{img.format}', fp=BytesIO(b_im))
+                        mbed = discord.Embed(
+                            title='Snap',
+                            color=color
+                        )
+                        mbed.set_image(url=f'attachment://5g1g.{img.format}')
+                        mbed.set_footer(text=f'Five Of You, One Of You | Syntax: p! 5g1g <guy> <girl>')
+                        await ctx.send(embed=mbed, file=file)
+                    else:
+                        await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Problem while snapping! | Response: {r.status}.', color=color))
 
     @swirl.error
     async def swirl_error(self, ctx, error):
@@ -227,6 +270,25 @@ class manipulation(commands.Cog):
 
     ## IMAGE EDITING AND PROCESSING
 
+    @commands.group(aliases=['rs'])
+    async def resize(self, ctx, url: str, width: int, height: int):
+        async with self.ses.get(url) as r:
+            if r.status in range(200, 299):
+                im = Image.open(BytesIO(await r.read()), mode='r')
+                im_res = im.resize((width, height))
+                b = BytesIO()
+                im_res.save(b, 'PNG')
+                b_im = b.getvalue()
+                file = discord.File(filename='resized.png', fp=BytesIO(b_im))
+                mbed = discord.Embed(
+                    title=f'Snap! | Image resized to {width}x{height}',
+                    color=color
+                )
+                mbed.set_image(url='attachment://resized.png')
+                await ctx.send(embed=mbed, file=file)
+            else:
+                await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Problem while snapping! Image may be a gif. | {r.status} response.', color=color))
+
     @commands.command(aliases=['rtt'])
     @commands.cooldown(rate=2, per=3, type=BucketType.user)
     async def rotate(self, ctx, url: str, degrees: int):
@@ -259,27 +321,68 @@ class manipulation(commands.Cog):
             await ctx.send(embed=errembed)
 
         elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(embed=discord.Embed(description='Syntax: p! rtt <image url> <degrees>'))
+            async with self.ses.get(str(ctx.author.avatar_url)) as r:
+                img = Image.open(BytesIO(await r.read()), mode='r')
+                try:
+                    img.seek(1)
+                    url = str(ctx.author.avatar_url_as(format=f'gif'))
+                except EOFError:
+                    url = str(ctx.author.avatar_url_as(format=f'png'))
 
-    """
-        @commands.group(aliases=['rs'])
-        async def resize(self, ctx, url: str, width: int, height: int):
-            async with self.ses.get(url) as r:
-                if r.status in range(200, 299):
-                    im = Image.open(BytesIO(await r.read()), mode='r')
-                    im_res = im.resize((width, height))
-                    b = BytesIO()
-                    im_res.save(b, 'PNG')
-                    b_im = b.getvalue()
-                    file = discord.File(filename='resized.png', fp=BytesIO(b_im))
-                    mbed = discord.Embed(
-                        title = f'Snap! | Image resized to {width}x{height}',
-                        color=color
-                    )
-                    mbed.set_image(url='attachment://resized.png')
-                    await ctx.send(embed=mbed, file=file)
-                else:
-                    await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Problem while snapping! Image may be a gif. | {r.status} response.', color=color))
-    """
+                async with self.ses.get(url) as r:
+                    if r.status in range(200, 299):
+                        im = Image.open(BytesIO(await r.read()), mode='r')
+                        im_rtt = im.rotate(angle=180)
+                        b = BytesIO()
+                        im_rtt.save(b, 'PNG')
+                        b_im = b.getvalue()
+                        file = discord.File(filename='rotated.png', fp=BytesIO(b_im))
+                        mbed = discord.Embed(
+                            title=f'Snap! | Automatically rotated by 180°',
+                            color=color
+                        )
+                        mbed.set_image(url='attachment://rotated.png')
+                        mbed.set_footer(text='Syntax: p! rtt <image link> <degrees>')
+                        await ctx.send(embed=mbed, file=file)
+                    else:
+                        await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Problem while snapping! Image may be a gif. | {r.status} response.', color=color))
+
+    @resize.error
+    async def rs_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            errembed = discord.Embed(
+                title='Hold on there, buddy',
+                color=err_color,
+                description='Wait 3 more seconds before you can get another snap!'
+            )
+            await ctx.send(embed=errembed)
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            async with self.ses.get(str(ctx.author.avatar_url)) as r:
+                img = Image.open(BytesIO(await r.read()), mode='r')
+                try:
+                    img.seek(1)
+                    url = str(ctx.author.avatar_url_as(format=f'gif'))
+                except EOFError:
+                    url = str(ctx.author.avatar_url_as(format=f'png'))
+
+                async with self.ses.get(url) as r:
+                    if r.status in range(200, 299):
+                        im = Image.open(BytesIO(await r.read()), mode='r')
+                        im_res = im.resize((500, 500))
+                        b = BytesIO()
+                        im_res.save(b, 'PNG')
+                        b_im = b.getvalue()
+                        file = discord.File(filename='resized.png', fp=BytesIO(b_im))
+                        mbed = discord.Embed(
+                            title=f'Snap! | Automatically resized to 500x500',
+                            color=color
+                        )
+                        mbed.set_image(url='attachment://resized.png')
+                        await ctx.send(embed=mbed, file=file)
+                    else:
+                        await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Problem while snapping! Image may be a gif. | {r.status} response.', color=color))
+
+
 def setup(bot):
     bot.add_cog(manipulation(bot))
