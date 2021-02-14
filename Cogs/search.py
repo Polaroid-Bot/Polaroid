4eb647ff13e90192afb93fdbe800843d325b822b
clvrk <jheru711@Gmail.com>
2021-02-14 18:55:45 -0400

    Added other useful parameters

--- Cogs/search.py
+++ Cogs/search.py
@@ -3,6 +3,7 @@ import os
 import aiohttp
 from discord.ext import commands
 from discord.ext.commands import BucketType
+import random
 
 color = 0x0da2ff
 
@@ -16,7 +17,8 @@ class search(commands.Cog):
     @commands.cooldown(rate=2, per=3, type=BucketType.user)
     async def search(self, ctx, *, query):
         query = query.replace(' ', '+')
-        async with self.ses.get(f"https://pixabay.com/api/?key={self.pixabay}&q={query}&image_type=photo&safesearch=true") as r:
+        page = [1, 2, 3, 4, 5]
+        async with self.ses.get(f"https://pixabay.com/api/?key={self.pixabay}&q={query}&image_type=photo&safesearch=true&page={random.choice(page)}") as r:
             if r.status in range(200, 299):
                 try:
                     data = await r.json()
@@ -40,7 +42,34 @@ class search(commands.Cog):
     @commands.cooldown(rate=2, per=3, type=BucketType.user)
     async def art(self, ctx, *, query):
         query = query.replace(' ', '+')
-        async with self.ses.get(f"https://pixabay.com/api/?key={self.pixabay}&q={query}&image_type=illustration&safesearch=true") as r:
+        page = [1, 2, 3, 4, 5]
+        async with self.ses.get(f"https://pixabay.com/api/?key={self.pixabay}&q={query}&image_type=illustration&safesearch=true&page={random.choice(page)}") as r:
+            if r.status in range(200, 299):
+                try:
+                    data = await r.json()
+                    image_url = data["hits"][0]["webformatURL"]
+                    views = data["hits"][0]["views"]
+                    likes = data["hits"][0]["likes"]
+                    mbed = discord.Embed(
+                        title='Snap!',
+                        color=color,
+                        description=f'[Link]({image_url})'
+                    )
+                    mbed.set_image(url=image_url)
+                    mbed.set_footer(text=f'👀 Views: {views} 👍 Likes: {likes}')
+                    await ctx.send(embed=mbed)
+                except IndexError:
+                    await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Problem while snapping! | Image not found.', color=color))
+            else:
+                await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Problem while snapping! | Response: {r.status}.', color=color))
+
+    @search.command(aliases=['wallpaper'])
+    @commands.cooldown(rate=2, per=3, type=BucketType.user)
+    async def wall(self, ctx, *, query):
+        query = query.replace(' ', '+')
+        page = [1, 2, 3, 4, 5]
+        type = ['illustration', 'photo']
+        async with self.ses.get(f"https://pixabay.com/api/?key={self.pixabay}&q={query}&image_type={random.choice(type)}&safesearch=true&category=backgrounds&page={random.choice(page)}") as r:
             if r.status in range(200, 299):
                 try:
                     data = await r.json()
