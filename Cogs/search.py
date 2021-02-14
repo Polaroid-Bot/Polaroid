5f87b8ccc7dcccf082a925952c51b3b07d3889f7
clvrk <jheru711@Gmail.com>
2021-02-14 19:15:44 -0400

    Done with search commands

--- Cogs/search.py
+++ Cogs/search.py
@@ -13,9 +13,48 @@ class search(commands.Cog):
         self.pixabay = os.getenv("PIXABAY")
         self.ses = bot.aiohttp_session
 
-    @commands.group()
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            errembed = discord.Embed(
+                title='Hold on there, buddy',
+                color=err_color,
+                description='Wait 3 more seconds before you can get another snap!'
+            )
+            await ctx.send(embed=errembed)
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Please pass in proper arguments. | Syntax: p! search art <query>', color=color))
+
+    @commands.group(invoke_without_command=True)
     @commands.cooldown(rate=2, per=3, type=BucketType.user)
     async def search(self, ctx, *, query):
+        query = query.replace(' ', '+')
+        page = [1, 2, 3, 4, 5]
+        type = ['illustration', 'photo']
+        async with self.ses.get(f"https://pixabay.com/api/?key={self.pixabay}&q={query}&image_type={random.choice(type)}&safesearch=true&page={random.choice(page)}") as r:
+            if r.status in range(200, 299):
+                try:
+                    data = await r.json()
+                    image_url = data["hits"][0]["webformatURL"]
+                    views = data["hits"][0]["views"]
+                    likes = data["hits"][0]["likes"]
+                    mbed = discord.Embed(
+                        title='Snap!',
+                        color=color,
+                        description=f'[Link]({image_url})'
+                    )
+                    mbed.set_image(url=image_url)
+                    mbed.set_footer(text=f'👀 Views: {views} 👍 Likes: {likes}')
+                    await ctx.send(embed=mbed)
+                except IndexError:
+                    await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Problem while snapping! | Image not found.', color=color))
+            else:
+                await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Problem while snapping! | Response: {r.status}.', color=color))
+
+    @search.command(aliases=['image'])
+    @commands.cooldown(rate=2, per=3, type=BucketType.user)
+    async def photo(self, ctx, *, query):
         query = query.replace(' ', '+')
         page = [1, 2, 3, 4, 5]
         async with self.ses.get(f"https://pixabay.com/api/?key={self.pixabay}&q={query}&image_type=photo&safesearch=true&page={random.choice(page)}") as r:
@@ -88,32 +127,6 @@ class search(commands.Cog):
                     await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Problem while snapping! | Image not found.', color=color))
             else:
                 await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Problem while snapping! | Response: {r.status}.', color=color))
-
-    @search.error
-    async def search_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            errembed = discord.Embed(
-                title='Hold on there, buddy',
-                color=err_color,
-                description='Wait 3 more seconds before you can get another snap!'
-            )
-            await ctx.send(embed=errembed)
-
-        elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Please pass in proper arguments. | Syntax: p! search <query>', color=color))
-
-    @art.error
-    async def art_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            errembed = discord.Embed(
-                title='Hold on there, buddy',
-                color=err_color,
-                description='Wait 3 more seconds before you can get another snap!'
-            )
-            await ctx.send(embed=errembed)
-
-        elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(embed=discord.Embed(description=f'<:error:806618798768652318> Please pass in proper arguments. | Syntax: p! search art <query>', color=color))
 
 
 def setup(bot):
